30cc72a315913004decc9818531b94555feb27ce
Opilane TTHK <opilane@tthk.ee>
2024-05-03 12:38:11 +0300

    twat

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ForLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("For loop");
+
+            int loop = 2;
+
+            for (int i = 0; i < loop; i++)
+            {
+                Console.WriteLine("Loop " + i);
+                Console.Beep();
+            }
+        }
+    }
+}
