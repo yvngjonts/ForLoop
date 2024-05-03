c4bcfe4e41e1b16375c9967e04d561430a5b5a57
Opilane TTHK <opilane@tthk.ee>
2024-05-03 12:46:11 +0300

    IfAndElse

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teha if ja else konsoolirakendus, kus " +
+                 "kontrollitakse stringi abil värvi vastavust");
+
+            Console.WriteLine("Värvide valikuks on: red, blue, green ja white");
+
+            Console.WriteLine("Peab käsitlema juhust, kus vastaja ei sisesta " +
+                "eelpool sisestatud värvi");
+
+            Console.WriteLine("Sisesta värv");
+            string varv = Console.ReadLine();
+
+            if (varv == "red")
+            {
+                Console.WriteLine("See on punane värv");
+            }
+            else if (varv == "blue")
+            {
+                Console.WriteLine("See on sinine");
+            }
+            else if (varv == "green")
+            {
+                Console.WriteLine("See on roheline");
+            }
+            else if (varv == "white")
+            {
+                Console.WriteLine("See on valge");
+            }
+            else
+            {
+                Console.WriteLine("Sisestasid suvalise värvi");
+            }
+        }
+    }
+}
